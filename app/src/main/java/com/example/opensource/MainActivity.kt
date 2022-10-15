da420418c7b4c44c21d231ade2435f50746e0b5d
yubinquitous <yb9854@naver.com>
2022-10-15 15:17:05 +0900

    init project

---
+++ app/src/main/java/com/example/opensource/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.opensource
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.opensource.databinding.ActivityMainBinding
+import com.example.opensource.save.SaveFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var navView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setBottomNavi()
+        setListeners()
+    }
+
+    private fun setBottomNavi() {
+        navView = binding.navView
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
+        binding.navView.background = null
+    }
+
+    private fun setListeners() {
+        binding.fabSave.bringToFront()
+        binding.fabSave.setOnClickListener {
+            SaveFragment().show(supportFragmentManager, "save")
+        }
+    }
+}
