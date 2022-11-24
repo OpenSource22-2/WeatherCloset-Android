0cb47768e63d25f809e6d4833f2cbe5f16ccfb8b
yubinquitous <yb9854@naver.com>
2022-11-24 11:38:50 +0900

    [FEAT] MyPageFragment 탭 구조 (#9)

--- app/src/main/java/com/example/opensource/my_page/MyPageFragment.kt
+++ app/src/main/java/com/example/opensource/my_page/MyPageFragment.kt
@@ -1,60 +1,48 @@
 package com.example.opensource.my_page
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.opensource.R
+import com.example.opensource.databinding.FragmentMyPageBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MypageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentMyPageBinding
+    private lateinit var tabViewPagerAdapter: MyPageTabViewPagerAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+
+        binding = FragmentMyPageBinding.inflate(inflater, container, false)
+
+        initAdapter()
+        initTabLayout()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MypageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initAdapter() {
+        val fragmentList = listOf(UserRecordFragment(), UserLikeFragment())
+
+        tabViewPagerAdapter = MyPageTabViewPagerAdapter(this)
+        tabViewPagerAdapter.fragments.addAll(fragmentList)
+
+        binding.vpHome.adapter = tabViewPagerAdapter
+    }
+
+    private fun initTabLayout() {
+        val tabIcon = listOf(R.drawable.ic_user_record_select, R.drawable.ic_user_like_select)
+
+        TabLayoutMediator(binding.tlUser, binding.vpHome) { tab, position ->
+            tab.setIcon(tabIcon[position])
+        }.attach()
     }
 }
