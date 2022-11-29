2faf0bea6c7af0b19245cfca11e1bc6fd8c520cd
yubinquitous <yb9854@naver.com>
2022-11-29 15:16:02 +0900

    Fix untracked files

--- app/src/main/java/com/example/opensource/Secret.kt
+++
@@ -1,8 +0,0 @@
-package com.example.opensource
-
-class Secret {
-    companion object {
-        const val API_KEY: String = "252119869b4a62c5670942f4531ea735"
-        const val BASE_URL = "http://15.164.59.9:8080/"
-    }
-}
