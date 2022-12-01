f2df465ebc6c4e1777478562408af4900dc0ad9d
Jiwon Lee <ljw010512@naver.com>
2022-12-01 23:45:22 +0900

    [UI] 캘린더뷰 위클리/먼슬리 UI 개선, 버튼 추가 (#3)

--- app/src/main/java/com/example/opensource/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/opensource/calendar/CalendarFragment.kt
@@ -1,20 +1,20 @@
 package com.example.opensource.calendar
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.opensource.MainActivity
 import com.example.opensource.R
+import com.example.opensource.data.remote.HomeRecordResponse
 import com.example.opensource.databinding.FragmentCalendarBinding
 import com.prolificinteractive.materialcalendarview.*
 
 
 class CalendarFragment : Fragment() {
 
-    private lateinit var widget: MaterialCalendarView
     private lateinit var userDailyRecord: View
     private lateinit var binding: FragmentCalendarBinding
     private var dateList = arrayListOf<CalendarDay>()
@@ -33,10 +33,20 @@ class CalendarFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         userDailyRecord = view.findViewById(R.id.userDailyRecord)
-        widget = view.findViewById(R.id.calendarView)
-
         setCalendarView()
     }
+
+    /*private fun setUserRecord(data: HomeRecordResponse.HomeRecordData) {
+        Glide.with(requireContext())
+            .load(data.imageUrl)
+            .into(binding.userDailyRecord.ivImage)
+        binding.userDailyRecord.tvTemperature.text = data.temperature.toString()
+        binding.userDailyRecord.tvUsermemo.text = data.comment
+        if (data.heart)
+            binding.userDailyRecord.ivHeart.setImageResource(R.drawable.heart_full)
+        else
+            binding.userDailyRecord.ivHeart.setImageResource(R.drawable.heart_empty)
+    }*/
 
     private fun setCalendarView(){
 
@@ -48,32 +58,43 @@ class CalendarFragment : Fragment() {
         dateList.add(CalendarDay.from(2022, 12, 1))
         dateList.add(CalendarDay.from(2023, 4, 30))
 
+        // 먼슬리, 위클리 변환
+        binding.icChange.setOnClickListener {
+            if(binding.calendarView.calendarMode==CalendarMode.MONTHS){
+                binding.calendarView.state().edit()
+                    .setCalendarDisplayMode(CalendarMode.WEEKS)
+                    .commit()
+                if(dateList.contains(binding.calendarView.selectedDate))
+                    userDailyRecord.setVisibility(View.VISIBLE)
+            }
+            else{
+                binding.calendarView.state().edit()
+                    .setCalendarDisplayMode(CalendarMode.MONTHS)
+                    .commit()
+                userDailyRecord.setVisibility(View.INVISIBLE)
+            }
+        }
 
         // 추가된 기록을 받아와 달력에 표시
-        widget.addDecorators(EventDecorator(dateList, activity))
+        binding.calendarView.addDecorators(EventDecorator(dateList, activity))
 
         // 특정 날짜를 선택한 경우 이벤트 처리
-        widget.setOnDateChangedListener(object: OnDateSelectedListener {
+        binding.calendarView.setOnDateChangedListener(object: OnDateSelectedListener {
             override fun onDateSelected(widget: MaterialCalendarView, date: CalendarDay, selected: Boolean) {
-                // 기록이 존재하는 경우, 유저 기록을 화면에 출력
+                // 기록이 존재하는 경우, (유저 기록을 화면에 출력)
                 if(dateList.contains(date)) {
-
                     widget.state().edit()
                         .setCalendarDisplayMode(CalendarMode.WEEKS)
                         .commit()
 
                     userDailyRecord.setVisibility(View.VISIBLE)
+                    binding.clPlz.visibility = View.INVISIBLE
                 }
                 else {
                     userDailyRecord.setVisibility(View.INVISIBLE)
-                }
-            }
-        })
+                    binding.clPlz.visibility = View.VISIBLE
 
-        // 선택한 달이 바뀌는 경우 유저 기록 화면 숨기기
-        widget.setOnMonthChangedListener(object: OnMonthChangedListener {
-            override fun onMonthChanged(widget: MaterialCalendarView?, date: CalendarDay?) {
-                userDailyRecord.setVisibility(View.INVISIBLE)
+                }
             }
         })
     }
